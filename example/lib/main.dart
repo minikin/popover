91f1f779aab205e62f926014a0e623c302835b7d
minikin <djminikin@gmail.com>
2021-01-27 14:06:22 +0100

    Update Readme, Example & Changelog

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:popover/popover.dart';
 
 void main() => runApp(PopoverExample());
@@ -64,71 +66,7 @@ class PopoverItems extends StatelessWidget {
       arrowWidth: 30,
       child: const Child(),
       onPop: () => print('Popover was popped!'),
-      bodyBuilder: (context) {
-        return Scrollbar(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 8, bottom: 8),
-            child: ListView(
-              padding: const EdgeInsets.all(8),
-              children: [
-                InkWell(
-                  onTap: () {
-                    print('GestureDetector was called on Entry A');
-                    Navigator.of(context).pop();
-                  },
-                  child: Container(
-                    height: 50,
-                    color: Colors.amber[100],
-                    child: const Center(child: Text('Entry A')),
-                  ),
-                ),
-                const Divider(),
-                Container(
-                  height: 50,
-                  color: Colors.amber[200],
-                  child: const Center(child: Text('Entry B')),
-                ),
-                const Divider(),
-                Container(
-                  height: 50,
-                  color: Colors.amber[300],
-                  child: const Center(child: Text('Entry C')),
-                ),
-                // const Divider(),
-                // Container(
-                //   height: 50,
-                //   color: Colors.amber[400],
-                //   child: const Center(child: Text('Entry D')),
-                // ),
-                // const Divider(),
-                // Container(
-                //   height: 50,
-                //   color: Colors.amber[500],
-                //   child: const Center(child: Text('Entry E')),
-                // ),
-                // const Divider(),
-                // Container(
-                //   height: 50,
-                //   color: Colors.amber[600],
-                //   child: const Center(child: Text('Entry F')),
-                // ),
-                // const Divider(),
-                // Container(
-                //   height: 50,
-                //   color: Colors.amber[700],
-                //   child: const Center(child: Text('Entry G')),
-                // ),
-                // const Divider(),
-                // Container(
-                //   height: 50,
-                //   color: Colors.amber[800],
-                //   child: const Center(child: Text('Entry H')),
-                // ),
-              ],
-            ),
-          ),
-        );
-      },
+      bodyBuilder: (context) => const ListItems(),
     );
   }
 }
@@ -156,6 +94,67 @@ class _ChildState extends State<Child> {
         onTap: () {
           print('GestureDetector was called in Child!');
         },
+      ),
+    );
+  }
+}
+
+class ListItems extends StatelessWidget {
+  const ListItems({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: [
+            InkWell(
+              onTap: () {
+                print('GestureDetector was called on Entry A');
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                height: 50,
+                color: Colors.amber[100],
+                child: const Center(child: Text('Entry A')),
+              ),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.amber[200],
+              child: const Center(child: Text('Entry B')),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.amber[300],
+              child: const Center(child: Text('Entry C')),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.amber[400],
+              child: const Center(child: Text('Entry D')),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry E')),
+            ),
+            const Divider(),
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry F')),
+            ),
+          ],
+        ),
       ),
     );
   }
